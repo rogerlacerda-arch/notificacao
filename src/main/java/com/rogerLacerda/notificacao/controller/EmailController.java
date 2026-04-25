8b0f5542924380a605f4f2a28d4a6054f30688b7
Roger Lacerda <lacerdarooh@gmail.com>
2026-04-25 15:37:46 -0300

    adicionados metodos de envio de email git push origin feature/servico_email git status rogerlacerda@MacBook-Air-de-Roger notificação % rogerlacerda@MacBook-Air-de-Roger notificação % git

---
+++ src/main/java/com/rogerLacerda/notificacao/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.rogerLacerda.notificacao.controller;
+
+import com.rogerLacerda.notificacao.business.EmailService;
+import com.rogerLacerda.notificacao.business.dto.TarefasDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping ("/email")
+
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<Void> enviarEmail(@RequestBody TarefasDTO dto){
+        emailService.enviaEmail(dto);
+        return ResponseEntity.ok().build();
+
+    }
+}
